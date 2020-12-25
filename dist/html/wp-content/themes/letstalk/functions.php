d4249dd02ac73e91dac1e6eb81f04ff4e87bbbf9
Joshua Coleman <jlcolema@me.com>
2020-12-25 17:47:28 -0600

    Remove call to set content width since this will be handled by the main stylesheet.

--- dist/html/wp-content/themes/letstalk/functions.php
+++ dist/html/wp-content/themes/letstalk/functions.php
@@ -82,25 +82,6 @@ if ( ! function_exists( 'lets_talk_setup' ) ) {
 add_action( 'after_setup_theme', 'lets_talk_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @since 1.0.0
- *
- * @global int $content_width Content width.
- *
- * @return void
- */
-function lets_talk_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'lets_talk_content_width', 750 );
-}
-add_action( 'after_setup_theme', 'lets_talk_content_width', 0 );
-
-/**
  * Enqueue scripts and styles.
  *
  * @since 1.0.0
