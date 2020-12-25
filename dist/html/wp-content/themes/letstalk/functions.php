ee9745bbb9e6a957721ee1877f2c6ccecdbfb6ae
Joshua Coleman <jlcolema@me.com>
2020-12-25 17:58:14 -0600

    Remove call for non-latin language styles since this will be handled by the main stylesheet.

--- dist/html/wp-content/themes/letstalk/functions.php
+++ dist/html/wp-content/themes/letstalk/functions.php
@@ -105,21 +105,6 @@ function lets_talk_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'lets_talk_scripts' );
 
-/** Enqueue non-latin language styles
- *
- * @since 1.0.0
- *
- * @return void
- */
-function lets_talk_non_latin_languages() {
-	$custom_css = lets_talk_get_non_latin_css( 'front-end' );
-
-	if ( $custom_css ) {
-		wp_add_inline_style( 'lets-talk-style', $custom_css );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'lets_talk_non_latin_languages' );
-
 // SVG Icons class.
 require get_template_directory() . '/classes/class-lets-talk-svg-icons.php';
 
