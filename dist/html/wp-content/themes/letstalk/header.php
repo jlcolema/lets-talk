47a399d9e5c06d8885f85cdc8f826f9cd3b8b842
Joshua Coleman <jlcolema@me.com>
2020-12-25 18:33:43 -0600

    Adjust attribute order.

--- dist/html/wp-content/themes/letstalk/header.php
+++ dist/html/wp-content/themes/letstalk/header.php
@@ -13,20 +13,30 @@
 
 ?>
 <!doctype html>
+
 <html <?php language_attributes(); ?> <?php letstalk_the_html_classes(); ?>>
+
 <head>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 	<?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'letstalk' ); ?></a>
 
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+	<header role="banner" class="header">
 
-	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+		<div class="header__inner-wrap">
+
+
+		</div>
+
+	</div>
+
+	<main role="main" class="main">
+
+		<div class="main__inner-wrap">
